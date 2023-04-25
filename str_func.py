d47c6c0d3ed7a7b4e6ef3ee1991fb4a136caf8bd
Sergy007 <kutsenkosergey@mail.ru>
2023-04-25 07:31:10 +0200

    создан файл str_func.py в нем функция upper_string

---
+++ str_func.py
@@ -0,0 +1,11 @@
+def upper_string():
+    '''Получаем строку и выводим её заглавными буквами'''
+    my_string = str(input())
+    up_str = my_string.upper()
+    return up_str
+
+def title_string():
+    '''Получаем строку и выводим все слова заглавными первыми буквами'''
+    t_string = str(input())
+    tit_string = t_string.title()
+    return tit_string
